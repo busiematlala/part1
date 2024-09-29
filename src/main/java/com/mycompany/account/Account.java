b3080c9916f718895d7a3ef4e008d24b9a5f4c1b
busie <busie@MatebookD15>
2024-09-29 17:56:34 +0200

    first commit

--- src/main/java/com/mycompany/account/Account.java
+++ src/main/java/com/mycompany/account/Account.java
@@ -134,12 +134,7 @@ else{
       Acc.userLast=input.next();
        System.out.println(Acc.registerUser());
  
-     if(Acc.checkUsername()==true && (Acc.checkPasswordComplexity())==true){
-     System.out.println("Enter your username");
-     Acc.user=input.next();
-       System.out.println("Enter your password");
-       Acc.pass=input.next();
-      System.out.println(Acc.returnLoginStatus()); 
+     
      
    
     }}}
